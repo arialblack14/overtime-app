b80bab4f7b79b20c4e8715a61a6df29f5af289a9
Alex Tsiras <tsirasalexis@yahoo.gr>
2017-02-10 20:42:15 +0200

    implement new index query for showing users posts they created and hiding others

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,6 +25,16 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = FactoryGirl.build_stubbed(:post)
+      other_user = FactoryGirl.create(:non_authorized_user)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post should not be seen", user_id: other_user.id)
+
+      visit posts_path
+
+      expect(page).to_not have_content(post_from_other_user.rationale)
+    end
   end
 
   describe 'new' do
@@ -39,9 +49,10 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      @post.update(user_id: @user.id)
       visit posts_path
 
-      click_link("delete_post_#{@post.id}from_index")
+      click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
